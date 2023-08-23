0b0c8498ffe95b07e6c07d02c851432c45a7b28e
Lavudin <142817450+Lavudin@users.noreply.github.com>
2023-08-23 20:39:48 +0200

    tabs

--- agent_code/custom_coin_collector/model.py
+++ agent_code/custom_coin_collector/model.py
@@ -11,7 +11,7 @@ class QLearning:
         self.num_actions = num_actions
         self.learning_rate = learning_rate
         self.discount_factor = discount_factor
-
+	    
 	self.total_reward = 0
 	self.exploration_prob = 1
         self.q_table = {}
