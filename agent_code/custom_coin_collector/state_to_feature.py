b81b908ec20d51db6c0c3ec474946ad743ddd13d
Nikita <ichich111222333@gmail.com>
2023-08-26 17:44:52 +0200

    State to feature V8

--- agent_code/custom_coin_collector/state_to_feature.py
+++ agent_code/custom_coin_collector/state_to_feature.py
@@ -280,6 +280,7 @@ def state_to_features_V7(game_state: dict) -> int:
     """
     Converts the game state into a number
     Only player location, walls arround him and nearest coin
+    Note: Words well but gets stuck too
     :param game_state:  A dictionary describing the current game board.
     :return: int
     """
@@ -313,7 +314,7 @@ def state_to_features_V7(game_state: dict) -> int:
     area_around_player = tuple(area_around_player.flatten())
     
 	# Add (TODO: try relative) coordinates of nearest coin(s)
-    nearestCoin = [0,0]
+    nearestCoin = [-1,-1]
     nearestCoinXdist = ARENA_LENGTH + 1
     nearestCoinYdist = ARENA_WIDTH  + 1
     
@@ -325,20 +326,6 @@ def state_to_features_V7(game_state: dict) -> int:
                 nearestCoinYdist = abs(coinY - playerY)
                 nearestCoin = i
 
-    '''
-	# Relative directino of coin to player
-    directionX = -1
-    directionY = -1
-    if nearestCoin[0] < playerX:
-         directionX = 0
-    else:
-         directionX = 1
-    if nearestCoin[1] < playerY:
-         directionY = 0
-    else:
-         directionY = 1
-    area_around_player += tuple([directionY, directionX])
-    '''
     # Relative directino of coin to player
     directionX = -1
     directionY = -1
@@ -355,6 +342,65 @@ def state_to_features_V7(game_state: dict) -> int:
         directionY = 1
     else:
         directionY = 2
+    area_around_player += tuple([directionY, directionX])
+	# Return hash value of area around player
+    key = hash(area_around_player)
+    return key
+
+def state_to_features_V8(game_state: dict) -> int:
+    """
+    Converts the game state into a number
+    Only player location, walls arround him and nearest coin
+    Note: Same as V7, but use rel distance instead of direction
+    :param game_state:  A dictionary describing the current game board.
+    :return: int
+    """
+    
+    field = game_state["field"].flatten()
+    coins = game_state["coins"]
+    player = game_state["self"]
+    
+	# Transform arena
+    field = [list_of_blocks.EMPTY.value if x == 0 else 
+             list_of_blocks.BRICK.value if x == -1 else
+             list_of_blocks.CRATE.value
+             for x in field]
+    
+	# Player cords
+    playerX = player[3][0]
+    playerY = player[3][1]
+
+	# Init area, only use odd sizes
+    area_size = 3
+    area_around_player = np.zeros((area_size, area_size))
+    
+    #Copy area around player
+    for i in range(0,area_size):
+         for j in range(0,area_size):
+              area_around_player[i][j] = field[(i-1+playerY)*ARENA_LENGTH + j-1 + playerX]
+         
+	# Add player to area
+    area_around_player[1][1] = list_of_blocks.PLAYER.value
+    
+    area_around_player = tuple(area_around_player.flatten())
+    
+	# Add (TODO: try relative) coordinates of nearest coin(s)
+    nearestCoin = [-1,-1]
+    nearestCoinXdist = ARENA_LENGTH + 1
+    nearestCoinYdist = ARENA_WIDTH  + 1
+    
+    for i in coins:
+         coinX = i[0]
+         coinY = i[1]
+         if abs(coinX - playerX) + abs(coinY - playerY) < nearestCoinXdist + nearestCoinYdist:
+                nearestCoinXdist = abs(coinX - playerX)
+                nearestCoinYdist = abs(coinY - playerY)
+                nearestCoin = i
+
+    # Relative directino of coin to player
+    directionX = nearestCoin[0] - playerX
+    directionY = nearestCoin[1] - playerY
+    
     area_around_player += tuple([directionY, directionX])
 	# Return hash value of area around player
     key = hash(area_around_player)
