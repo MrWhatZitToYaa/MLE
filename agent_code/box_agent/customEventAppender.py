7b07b91535288c161d104e8262bd28354b7c36cc
Lavudin <blanca@BlancasPC.localdomain>
2023-09-02 20:21:23 +0200

    fixed error

--- agent_code/box_agent/customEventAppender.py
+++ agent_code/box_agent/customEventAppender.py
@@ -9,7 +9,7 @@ def appendCustomEvents(self, events, new_game_state, old_game_state):
             events.append(COIN_DIST_DECREASED)
             self.logger.debug(f'Custom event occurred: {COIN_DIST_DECREASED}')
             
-    if is_coin_dist_decreased(old_game_state, new_game_state):
+    if is_bomb_dist_increased(old_game_state, new_game_state):
             events.append(BOMB_DIST_INCREASED)
             self.logger.debug(f'Custom event occurred: {BOMB_DIST_INCREASED}')
             
@@ -60,8 +60,8 @@ def is_bomb_dist_increased(old_state, new_state):
     """
     Checks whether the agent moved away from bomb.
     """
-    old_max_d = get_min_bomb_relative_coordinate(old_state["bombs"], old_state["field"], old_state["self"][3])
-    new_max_d = get_min_bomb_relative_coordinate(new_state["bombs"], new_state["field"], new_state["self"][3])
+    old_max_d = get_min_bomb_relative_coordinate(old_state["bombs"], old_state["field"], old_state["self"])
+    new_max_d = get_min_bomb_relative_coordinate(new_state["bombs"], new_state["field"], new_state["self"])
 
     return new_max_d > old_max_d
 
