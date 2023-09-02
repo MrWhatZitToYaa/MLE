9837f88e1984bc58f3cf518fd66ce21ba620edee
Lavudin <142817450+Lavudin@users.noreply.github.com>
2023-09-02 22:11:48 +0200

    fixed error

--- agent_code/neural_stuff_agent/model.py
+++ agent_code/neural_stuff_agent/model.py
@@ -8,11 +8,11 @@ from .definitions import *
 class Model(nn.Module):
     def __init__(self, input_channels, num_classes):
         super(Model, self).__init__()
-        model = nn.Sequential()
+        self.model = nn.Sequential()
         channels = [input_channels, 64, 32, num_classes]
 
         for i in range(len(channels)-1):
-            model.add_module(
+            self.model.add_module(
                 f'conv{i}',
                 nn.Linear(channels[i], channels[i+1])
             )
