6710e7026e4a4ee84bfffe46a709385bd887701b
Katharina Altrichter <kaa4762@thi.de>
2023-09-06 20:17:24 +0200

    cleanup

--- agent_code/neural_agent_act_rule/callbacks.py
+++ agent_code/neural_agent_act_rule/callbacks.py
@@ -14,7 +14,6 @@ def setup(self):
     :param self: This object is passed to all callbacks and you can set arbitrary values.
     """
     self.count = 0
-
     if self.train or not os.path.isfile("my-saved-model.pt"):
         self.logger.info("Setting up model from scratch.")
         weights = np.random.rand(len(ACTIONS))
@@ -37,35 +36,19 @@ def act(self, game_state: dict) -> str:
     :param game_state: The dictionary that describes everything on the board.
     :return: The action to take as a string.
     """
-    # Exploration vs exploitation
-    # fix exploration prob in model
-    '''
-    exploration_prob = 0.9
-    if self.train and random.random() < exploration_prob:
-        self.logger.debug("Choosing action purely at random.")
-        return np.random.choice(ACTIONS, p=PROBABILITIES_FOR_ACTIONS)
-
-    self.logger.debug("Querying model for action.")
-    state = state_to_features(game_state)
-    prob = self.model.forward(state).detach().numpy()
-    #print(prob)
-    #action = np.random.choice(ACTIONS, p=prob)
-    #print(ACTIONS[np.argmax(prob)])
-    return ACTIONS[np.argmax(prob)]
-    '''
 
     self.count += 1
     exploration_prob = STARTING_EXPLORATION_PROBABILITY
     if self.count % DECAY_AFTER_ROUNDS == 0:
         exploration_prob *= EPSILON_DECAY
 
-    state = state_to_features(game_state)
     if self.train and random.random() < exploration_prob:
-        return act_rule(game_state)
+        return act_rule(self, game_state)
     self.logger.debug("Querying model for action.")
-    res = np.random.choice(ACTIONS, p=F.softmax(self.model.forward(state), dim=0).detach().numpy())
+    res = np.random.choice(ACTIONS, p=F.softmax(self.model.forward(state_to_features(game_state)), dim=0).detach().numpy())
     # print(res)
     return res
+
 
 def look_for_targets(free_space, start, targets, logger=None):
     """Find direction of closest target that can be reached via free tiles.
@@ -97,7 +80,7 @@ def look_for_targets(free_space, start, targets, logger=None):
             best = current
             best_dist = d + dist_so_far[current]
         if d == 0:
-            # Found path to a target's exact position
+            # Found path to a target's exact position, mission accomplished!
             best = current
             break
         # Add unexplored free neighboring tiles to the queue in a random order
@@ -115,13 +98,12 @@ def look_for_targets(free_space, start, targets, logger=None):
     while True:
         if parent_dict[current] == start: return current
         current = parent_dict[current]
-def act_rule(game_state):
+def act_rule(self, game_state):
+    # Check if we are in a different round
     """
-    Called each game step to determine the agent's next action.
-
-    You can find out about the state of the game environment via game_state,
-    which is a dictionary. Consult 'get_state_for_agent' in environment.py to see
-    what it contains.
+    if game_state["round"] != self.current_round:
+        reset_self(self)
+        self.current_round = game_state["round"]
     """
     # Gather information about the game state
     arena = game_state['field']
@@ -135,13 +117,20 @@ def act_rule(game_state):
         for (i, j) in [(xb + h, yb) for h in range(-3, 4)] + [(xb, yb + h) for h in range(-3, 4)]:
             if (0 < i < bomb_map.shape[0]) and (0 < j < bomb_map.shape[1]):
                 bomb_map[i, j] = min(bomb_map[i, j], t)
-
+    """
+    # If agent has been in the same location three times recently, it's a loop
+    if self.coordinate_history.count((x, y)) > 2:
+        self.ignore_others_timer = 5
+    else:
+        self.ignore_others_timer -= 1
+    self.coordinate_history.append((x, y))
+    """
     # Check which moves make sense at all
     directions = [(x, y), (x + 1, y), (x - 1, y), (x, y + 1), (x, y - 1)]
     valid_tiles, valid_actions = [], []
     for d in directions:
         if ((arena[d] == 0) and
-                (game_state['explosion_map'][d] <= 1) and
+                (game_state['explosion_map'][d] < 1) and
                 (bomb_map[d] > 0) and
                 (not d in others) and
                 (not d in bomb_xys)):
@@ -152,28 +141,46 @@ def act_rule(game_state):
     if (x, y + 1) in valid_tiles: valid_actions.append('DOWN')
     if (x, y) in valid_tiles: valid_actions.append('WAIT')
 
+    """
+    # Disallow the BOMB action if agent dropped a bomb in the same spot recently
+    if (bombs_left > 0) and (x, y) not in self.bomb_history: valid_actions.append('BOMB')
+    self.logger.debug(f'Valid actions: {valid_actions}')
+    """
     # Collect basic action proposals in a queue
     # Later on, the last added action that is also valid will be chosen
     action_ideas = ['UP', 'DOWN', 'LEFT', 'RIGHT']
     random.shuffle(action_ideas)
 
     # Compile a list of 'targets' the agent should head towards
-    dead_ends = [(x, y) for x in range(1, 16) for y in range(1, 16) if (arena[x, y] == 0)
+    cols = range(1, arena.shape[0] - 1)
+    rows = range(1, arena.shape[0] - 1)
+    dead_ends = [(x, y) for x in cols for y in rows if (arena[x, y] == 0)
                  and ([arena[x + 1, y], arena[x - 1, y], arena[x, y + 1], arena[x, y - 1]].count(0) == 1)]
-    crates = [(x, y) for x in range(1, 16) for y in range(1, 16) if (arena[x, y] == 1)]
+    crates = [(x, y) for x in cols for y in rows if (arena[x, y] == 1)]
     targets = coins + dead_ends + crates
+    # Add other agents as targets if in hunting mode or no crates/coins left
+    if (len(crates) + len(coins) == 0):
+        targets.extend(others)
 
     # Exclude targets that are currently occupied by a bomb
     targets = [targets[i] for i in range(len(targets)) if targets[i] not in bomb_xys]
 
     # Take a step towards the most immediately interesting target
-    free_space = arena == 0
 
-    d = look_for_targets(free_space, (x, y), targets, None)
+    free_space = arena == 0
+    """
+    if self.ignore_others_timer > 0:
+        for o in others:
+            free_space[o] = False
+    """
+    d = look_for_targets(free_space, (x, y), targets, self.logger)
     if d == (x, y - 1): action_ideas.append('UP')
     if d == (x, y + 1): action_ideas.append('DOWN')
     if d == (x - 1, y): action_ideas.append('LEFT')
     if d == (x + 1, y): action_ideas.append('RIGHT')
+    if d is None:
+        self.logger.debug('All targets gone, nothing to do anymore')
+        action_ideas.append('WAIT')
 
     # Add proposal to drop a bomb if at dead end
     if (x, y) in dead_ends:
@@ -211,4 +218,7 @@ def act_rule(game_state):
     while len(action_ideas) > 0:
         a = action_ideas.pop()
         if a in valid_actions:
+            # Keep track of chosen action for cycle detection
+            # if a == 'BOMB':
+                #self.bomb_history.append((x, y))
             return a
---
+++ agent_code/neural_agent_pred_exp_state/callbacks.py
@@ -0,0 +1,50 @@
+import numpy as np
+import os
+import random
+import pickle
+import torch.nn.functional as torch_functions
+
+from .definitions import *
+from .model import *
+from .state_to_feature import state_to_features
+
+def setup(self):
+    """
+    Copy pasted setup method
+    :param self: This object is passed to all callbacks and you can set arbitrary values.
+    """
+    self.count = 0
+    if self.train or not os.path.isfile("my-saved-model.pt"):
+        self.logger.info("Setting up model from scratch.")
+        weights = np.random.rand(len(ACTIONS))
+        self.model = Model(INPUT_CHANNELS, NUM_OF_ACTIONS)
+    else:
+        self.logger.info("Loading model from saved state.")
+        with open("my-saved-model.pt", "rb") as file:
+            self.model = pickle.load(file)
+            
+	# Keeps track of the scores if evaluation mode is active
+    self.scores = []
+
+
+def act(self, game_state: dict) -> str:
+    """
+    Your agent should parse the input, think, and take a decision.
+    When not in training mode, the maximum execution time for this method is 0.5s.
+
+    :param self: The same object that is passed to all of your callbacks.
+    :param game_state: The dictionary that describes everything on the board.
+    :return: The action to take as a string.
+    """
+
+    self.count += 1
+    exploration_prob = STARTING_EXPLORATION_PROBABILITY
+    if self.count % DECAY_AFTER_ROUNDS == 0:
+        exploration_prob *= EPSILON_DECAY
+
+    if self.train and random.random() < exploration_prob:
+        return np.random.choice(ACTIONS, p=PROBABILITIES_FOR_ACTIONS)
+    self.logger.debug("Querying model for action.")
+    res = np.random.choice(ACTIONS, p=F.softmax(self.model.forward(state_to_features(game_state)), dim=0).detach().numpy())
+    # print(res)
+    return res
